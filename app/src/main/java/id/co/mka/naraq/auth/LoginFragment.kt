93606b475535fa15af5ff3c891004caa0646d9c9
Ardhy Satrio <rio27dh@gmail.com>
2022-09-30 10:24:38 +0800

    add: login input validation

--- app/src/main/java/id/co/mka/naraq/auth/LoginFragment.kt
+++ app/src/main/java/id/co/mka/naraq/auth/LoginFragment.kt
@@ -1,11 +1,15 @@
 package id.co.mka.naraq.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import id.co.mka.naraq.R
 import id.co.mka.naraq.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -26,8 +30,86 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        toggleButton()
+        inputListener()
+        setupAction()
+    }
+    private fun setupAction() {
         binding?.tvRegisterSwitch?.setOnClickListener {
             findNavController().navigate(LoginFragmentDirections.actionNavigationLoginToNavigationRegister())
         }
+        binding?.btnLogin?.setOnClickListener {
+            if (validateInput()) {
+                // do smth
+            }
+        }
+    }
+
+    private fun inputListener() {
+        binding?.apply {
+            inputEmail.doAfterTextChanged {
+                toggleButton()
+                binding?.inputLayoutEmail?.error = null
+            }
+            inputPassword.doAfterTextChanged {
+                toggleButton()
+                binding?.inputLayoutPassword?.error = null
+            }
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        binding?.inputLayoutEmail?.error = validEmail()
+        binding?.inputLayoutPassword?.error = validPassword()
+
+        val validEmail = binding?.inputLayoutEmail?.helperText == null
+        val validPassword = binding?.inputLayoutPassword?.helperText == null
+
+        return validEmail && validPassword
+    }
+
+    private fun toggleButton() {
+        val inputEmail = binding?.inputEmail?.text?.isNotEmpty()
+        val inputPassword = binding?.inputPassword?.text?.isNotEmpty()
+        val enabledBackground = ContextCompat.getDrawable(requireContext(), R.drawable.bg_button)
+        val disabledBackground = ContextCompat.getDrawable(requireContext(), R.drawable.bg_button_disabled)
+
+        if (inputEmail == true && inputPassword == true) {
+            binding?.btnLogin?.background = enabledBackground
+            binding?.btnLogin?.isEnabled = true
+        } else {
+            binding?.btnLogin?.background = disabledBackground
+            binding?.btnLogin?.isEnabled = false
+        }
+    }
+
+    private fun validPassword(): String? {
+        val passwordText = binding?.inputPassword?.text.toString()
+        if (passwordText.length < 8) {
+            return "Minimum 8 Character Password"
+        }
+        if (!passwordText.matches(".*[A-Z].*".toRegex())) {
+            return "Must Contain 1 Upper-case Character"
+        }
+        if (!passwordText.matches(".*[a-z].*".toRegex())) {
+            return "Must Contain 1 Lower-case Character"
+        }
+        if (!passwordText.matches(".*[@#\$%^&+=].*".toRegex())) {
+            return "Must Contain 1 Special Character (@#\$%^&+=)"
+        }
+        return null
+    }
+
+    private fun validEmail(): String? {
+        val emailText = binding?.inputEmail?.text.toString()
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
+            return "Invalid Email Address"
+        }
+        return null
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
